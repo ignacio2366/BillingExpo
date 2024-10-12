38f7a7728e4641e55cc225a07f65526a506d068b
mark angelo <76676425+ignacio2366@users.noreply.github.com>
2024-10-13 00:41:00 +0800

    initial commit

---
+++ app/Admin/default.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+function MyComponent(): JSX.Element {
+  return (
+    <View>
+      <Text>Admin</Text>
+    </View>
+  );
+}
+export default MyComponent;
